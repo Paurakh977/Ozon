598275d3de3880b8938d15d503290bbbbe4e275e
Paurakh Raj Pandey <124509859+Paurakh977@users.noreply.github.com>
2026-03-12 22:51:36 +0545

    tweaking tavly default prams for better results

--- agent_server/test_agent/agent.py
+++ agent_server/test_agent/agent.py
@@ -6,7 +6,7 @@ from google.adk.tools.mcp_tool import McpToolset
 from google.adk.tools.mcp_tool.mcp_session_manager import StdioConnectionParams
 from google.adk.tools.mcp_tool.mcp_session_manager import StreamableHTTPServerParams
 from mcp import StdioServerParameters
-import os
+import os,json
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -140,6 +140,22 @@ Mercury_2_model= LiteLlm(
         "reasoning_effort": "instant",  
     }
 )
+
+DEFAULT_PARAMS = json.dumps({
+    # docs for tewaking prams for better results with the tavily web search tool
+    # https://docs.tavily.com/documentation/api-reference/endpoint/search
+    # https://docs.tavily.com/documentation/best-practices/best-practices-search#search-depth
+    
+    "max_results": 3,
+    "search_depth": "basic",  #basic and ultra-fast also an option
+    "include_raw_content": False,
+    "include_favicon": False,
+    "include_images": False,
+    "include_answer":True,
+    # "exclude_domains":True,
+    "auto_parameters":False,
+    "exact_match": True,
+})
 
 root_agent = LlmAgent(
     name="Mercury2_agent",
